ba0fb40966563a319e2261b43b48b28753e3d355
skeleton2010 <inseong747@gmail.com>
2023-01-11 07:45:19 +0900

    돔을 이용하여 HTML객체 저장

--- app/src/public/js/report.js
+++ app/src/public/js/report.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("Hello World!");
+const email = document.querySelector("#email"),
+    inGameID = document.querySelector("#inGameID"),
+    subject = document.querySelector("#subject"),
+    description = document.querySelector("#description"),
+    submitBtn = document.querySelector("button");
+
+submitBtn.addEventListener("click", submit);
+
+function submit() {
+    const req = {
+        email: email.value,
+        inGameID: inGameID.value,
+        subject: subject.value,
+        description: description.value,
+    };
+    console.log(req);
+}
